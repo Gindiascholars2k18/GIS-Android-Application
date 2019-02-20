136638fb673658632140c51104654798a2f04970
sdzshn3 <sdzshn3@gmail.com>
2019-02-20 15:18:01 +0530

    Migrated to AndroidX

--- app/src/main/java/com/Gindiascholars2k18/MainActivity.java
+++ app/src/main/java/com/Gindiascholars2k18/MainActivity.java
@@ -1,6 +1,6 @@
 package com.Gindiascholars2k18;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {

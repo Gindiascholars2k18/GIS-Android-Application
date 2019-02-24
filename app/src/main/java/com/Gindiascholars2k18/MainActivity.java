8f768d60d6cd0ea760b202dac741fd889d403c22
sdzshn3 <sdzshn3@gmail.com>
2019-02-24 17:34:23 +0530

    Changed Nav drawer icons and activity theme

--- app/src/main/java/com/Gindiascholars2k18/MainActivity.java
+++ app/src/main/java/com/Gindiascholars2k18/MainActivity.java
@@ -3,6 +3,7 @@ package com.Gindiascholars2k18;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import butterknife.ButterKnife;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -14,26 +15,31 @@ import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
 import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String PROFILES = "1", PORTFOLIO = "2", RESUME = "3", FEEDBACK = "4", ABOUT = "5", SETTINGS = "6", CONTACT_US = "7", RATE_APP = "8", SHARE_APP = "9";
-    private Drawer result = null;
-    private String lastSelectedItem = PROFILES;
-
+    private final String PROFILES = "1", PORTFOLIO = "2", RESUME = "3", FEEDBACK = "4",
+            ABOUT = "5", SETTINGS = "6", CONTACT_US = "7", RATE_APP = "8", SHARE_APP = "9",
+            PROFILE = "10", MANAGE_ACCOUNT = "11";
+    private Drawer drawer = null;
+    private AccountHeader accountHeader = null;
+    private String lastSelectedDrawerItem = PROFILES;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         // Handle Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(R.string.app_name);
-
-        AccountHeader headerResult = new AccountHeaderBuilder()
+        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.app_name);
+        accountHeader = new AccountHeaderBuilder()
                 .withActivity(this)
                 .withTextColor(Color.WHITE)
                 .addProfiles(
@@ -41,68 +47,110 @@ public class MainActivity extends AppCompatActivity {
                                 .withName("User Name")
                                 .withEmail("user@email.com")
                                 .withIcon(R.drawable.user_image)
+                                .withIdentifier(Long.parseLong(PROFILE)),
+                        new ProfileSettingDrawerItem()
+                                .withName("Manage Account")
+                                .withIcon(R.drawable.ic_settings)
+                                .withIdentifier(Long.parseLong(MANAGE_ACCOUNT))
                 )
                 .withHeaderBackground(R.drawable.wallpaper)
-                .withOnAccountHeaderListener((view, profile, current) -> false)
+                .withOnAccountHeaderListener((view, profile, current) -> {
+                    switch (String.valueOf(profile.getIdentifier())) {
+                        case PROFILE:
+                            Toast.makeText(this, "Clicked on " + profile.getName(), Toast.LENGTH_SHORT).show();
+                            break;
+                        case MANAGE_ACCOUNT:
+                            Toast.makeText(this, "Clicked on " + profile.getName(), Toast.LENGTH_SHORT).show();
+                    }
+                    return false;
+                })
+                .withSavedInstance(savedInstanceState)
                 .build();
 
         //Create the drawer
-        result = new DrawerBuilder()
+        drawer = new DrawerBuilder()
                 .withActivity(this)
-                .withAccountHeader(headerResult)
+                .withAccountHeader(accountHeader)
                 .withDisplayBelowStatusBar(true)
                 .withActionBarDrawerToggleAnimated(true)
                 .withToolbar(toolbar)
                 .addDrawerItems(
-                        new PrimaryDrawerItem().withName("Profiles").withIdentifier(Long.parseLong(PROFILES)).withIcon(R.drawable.ic_profiles),
-                        new PrimaryDrawerItem().withName("Portfolio").withIdentifier(Long.parseLong(PORTFOLIO)).withIcon(R.drawable.ic_portfolio),
-                        new PrimaryDrawerItem().withName("Resume").withIdentifier(Long.parseLong(RESUME)).withIcon(R.drawable.ic_resume),
-                        new PrimaryDrawerItem().withName("Feedback").withIdentifier(Long.parseLong(FEEDBACK)).withIcon(R.drawable.ic_feedback),
-                        new PrimaryDrawerItem().withName("About").withIdentifier(Long.parseLong(ABOUT)).withIcon(R.drawable.ic_info),
-                        new SectionDrawerItem().withName("Tools"),
-                        new PrimaryDrawerItem().withName("Settings").withIdentifier(Long.parseLong(SETTINGS)).withIcon(R.drawable.ic_settings),
-                        new PrimaryDrawerItem().withName("Contact us").withIdentifier(Long.parseLong(CONTACT_US)).withIcon(R.drawable.ic_contact_us),
-                        new PrimaryDrawerItem().withName("Rate App").withIdentifier(Long.parseLong(RATE_APP)).withIcon(R.drawable.ic_rating),
-                        new PrimaryDrawerItem().withName("Share App").withIdentifier(Long.parseLong(SHARE_APP)).withIcon(R.drawable.ic_share)
-                ) // add the items we want to use with our Drawer
+                        new PrimaryDrawerItem()
+                                .withName("Profiles")
+                                .withIdentifier(Long.parseLong(PROFILES))
+                                .withIcon(R.drawable.ic_profiles),
+                        new PrimaryDrawerItem()
+                                .withName("Portfolio")
+                                .withIdentifier(Long.parseLong(PORTFOLIO))
+                                .withIcon(R.drawable.ic_portfolio),
+                        new PrimaryDrawerItem()
+                                .withName("Resume")
+                                .withIdentifier(Long.parseLong(RESUME))
+                                .withIcon(R.drawable.ic_resume),
+                        new PrimaryDrawerItem()
+                                .withName("Feedback")
+                                .withIdentifier(Long.parseLong(FEEDBACK))
+                                .withIcon(R.drawable.ic_feedback),
+                        new PrimaryDrawerItem().withName("About")
+                                .withIdentifier(Long.parseLong(ABOUT))
+                                .withIcon(R.drawable.ic_info),
+                        new SectionDrawerItem()
+                                .withName("Tools"),
+                        new PrimaryDrawerItem()
+                                .withName("Settings")
+                                .withIdentifier(Long.parseLong(SETTINGS))
+                                .withIcon(R.drawable.ic_settings),
+                        new PrimaryDrawerItem()
+                                .withName("Contact us")
+                                .withIdentifier(Long.parseLong(CONTACT_US))
+                                .withIcon(R.drawable.ic_contact_us),
+                        new PrimaryDrawerItem()
+                                .withName("Rate App")
+                                .withIdentifier(Long.parseLong(RATE_APP))
+                                .withIcon(R.drawable.ic_rating),
+                        new PrimaryDrawerItem()
+                                .withName("Share App")
+                                .withIdentifier(Long.parseLong(SHARE_APP))
+                                .withIcon(R.drawable.ic_share)
+                )// add the items we want to use with our Drawer
                 .withOnDrawerItemClickListener((view, position, drawerItem) -> {
                     long identifier = drawerItem.getIdentifier();
                     switch (String.valueOf(identifier)) {
                         case PROFILES:
                             Toast.makeText(MainActivity.this, "Profiles", Toast.LENGTH_SHORT).show();
-                            lastSelectedItem = PROFILES;
+                            lastSelectedDrawerItem = PROFILES;
                             break;
                         case PORTFOLIO:
                             Toast.makeText(MainActivity.this, "Portfolio", Toast.LENGTH_SHORT).show();
-                            lastSelectedItem = PORTFOLIO;
+                            lastSelectedDrawerItem = PORTFOLIO;
                             break;
                         case RESUME:
                             Toast.makeText(MainActivity.this, "Resume", Toast.LENGTH_SHORT).show();
-                            lastSelectedItem = RESUME;
+                            lastSelectedDrawerItem = RESUME;
                             break;
                         case FEEDBACK:
                             Toast.makeText(MainActivity.this, "Feedback", Toast.LENGTH_SHORT).show();
-                            lastSelectedItem = FEEDBACK;
+                            lastSelectedDrawerItem = FEEDBACK;
                             break;
                         case ABOUT:
                             Toast.makeText(MainActivity.this, "About", Toast.LENGTH_SHORT).show();
-                            lastSelectedItem = ABOUT;
+                            lastSelectedDrawerItem = ABOUT;
                             break;
                         case SETTINGS:
                             Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
-                            result.setSelection(Long.parseLong(lastSelectedItem), false);
+                            drawer.setSelection(Long.parseLong(lastSelectedDrawerItem), false);
                             break;
                         case CONTACT_US:
                             Toast.makeText(MainActivity.this, "Contact us", Toast.LENGTH_SHORT).show();
-                            result.setSelection(Long.parseLong(lastSelectedItem), false);
+                            drawer.setSelection(Long.parseLong(lastSelectedDrawerItem), false);
                             break;
                         case RATE_APP:
                             Toast.makeText(MainActivity.this, "Rate App", Toast.LENGTH_SHORT).show();
-                            result.setSelection(Long.parseLong(lastSelectedItem), false);
+                            drawer.setSelection(Long.parseLong(lastSelectedDrawerItem), false);
                             break;
                         case SHARE_APP:
                             Toast.makeText(MainActivity.this, "Share App", Toast.LENGTH_SHORT).show();
-                            result.setSelection(Long.parseLong(lastSelectedItem), false);
+                            drawer.setSelection(Long.parseLong(lastSelectedDrawerItem), false);
                             break;
                     }
                     return false;
@@ -114,15 +162,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         //add the values which need to be saved from the drawer to the bundle
-        outState = result.saveInstanceState(outState);
+        outState = drawer.saveInstanceState(outState);
+        outState = accountHeader.saveInstanceState(outState);
         super.onSaveInstanceState(outState);
     }
 
     @Override
     public void onBackPressed() {
         //handle the back press :D close the drawer first and if the drawer is closed close the activity
-        if (result != null && result.isDrawerOpen()) {
-            result.closeDrawer();
+        if (accountHeader != null && accountHeader.isSelectionListShown()) {
+            accountHeader.toggleSelectionList(MainActivity.this);
+            return;
+        }
+        if (drawer != null && drawer.isDrawerOpen()) {
+            drawer.closeDrawer();
         } else {
             super.onBackPressed();
         }
